2ce5cf0242072416dc0384cb4b8dc4722960d5d2
JENS\Jens <je.harder@gmail.com>
2017-05-25 20:32:07 +0200

    implemented strategyRunAwayGreedy (for men)

--- src/core/strategy/StrategyRunAwayGreedy.java
+++ src/core/strategy/StrategyRunAwayGreedy.java
@@ -9,17 +9,16 @@ import core.graph.Position;
 import java.util.ArrayList;
 
 /**
- * Created by Jens on 20.05.2017.
+ * Created by Jens on 25.05.2017.
  */
-public class StrategyAggro implements Strategy{
+public class StrategyRunAwayGreedy implements Strategy{
     @Override
     public Position getNextPosition(Entity e, ArrayList<Man> men, ArrayList<Lion> lions) {
-
         GraphHelper helper = GraphHelper.createGraphHelper();
 
         Position currentPosition = e.getCurrentPosition();
 
-        int bestSteps = helper.BFS(currentPosition);
+        int bestSteps = helper.BFS(currentPosition, true);
 
         for(Position neighborPosition : currentPosition.getAllNeighborPositions()){
             int steps = helper.BFS(currentPosition, neighborPosition);

1f6ea2fcc9dc83288dc6c7f24ffedad1c3c6fcfb
Danny Rademacher <danny.rademacher@gmail.com>
2017-06-17 16:00:30 +0200

    new standard graph

--- src/core/CoreController.java
+++ src/core/CoreController.java
@@ -6,6 +6,7 @@ import core.graph.Graph;
 import core.strategy.StrategyAggroGreedy;
 import core.strategy.StrategyRandom;
 import core.strategy.StrategyRunAwayGreedy;
+import core.strategy.StretegyAggroClever;
 import core.util.Point;
 
 import java.io.BufferedReader;
@@ -34,17 +35,74 @@ public class CoreController {
 
     public Graph setDefaultGraph1() {
         this.graph = new Graph();
-        // this.graph.registerVertex(new Point(5, 5));
+
+        this.graph.registerVertex(new Point(5, 2));
+        this.graph.registerVertex(new Point(19, 2));
+        this.graph.registerVertex(new Point(22, 14));
+        this.graph.registerVertex(new Point(12, 22));
+        this.graph.registerVertex(new Point(2, 14));
+
+        this.graph.registerVertex(new Point(12, 4));
+        this.graph.registerVertex(new Point(16, 5));
         this.graph.registerVertex(new Point(19, 9));
-        this.graph.registerVertex(new Point(9, 4));
+        this.graph.registerVertex(new Point(19, 13));
+        this.graph.registerVertex(new Point(16, 17));
+        this.graph.registerVertex(new Point(12, 18));
+        this.graph.registerVertex(new Point(8, 17));
+        this.graph.registerVertex(new Point(5, 13));
+        this.graph.registerVertex(new Point(5, 9));
+        this.graph.registerVertex(new Point(8, 5));
+
+        this.graph.registerVertex(new Point(12, 7));
+        this.graph.registerVertex(new Point(15, 10));
+        this.graph.registerVertex(new Point(14, 14));
+        this.graph.registerVertex(new Point(10, 14));
+        this.graph.registerVertex(new Point(9, 10));
+
         this.graph.registerEdge(this.graph.getVertices().get(0), this.graph.getVertices().get(1));
+        this.graph.registerEdge(this.graph.getVertices().get(1), this.graph.getVertices().get(2));
+        this.graph.registerEdge(this.graph.getVertices().get(2), this.graph.getVertices().get(3));
+        this.graph.registerEdge(this.graph.getVertices().get(3), this.graph.getVertices().get(4));
+        this.graph.registerEdge(this.graph.getVertices().get(4), this.graph.getVertices().get(0));
+
+        this.graph.registerEdge(this.graph.getVertices().get(0), this.graph.getVertices().get(14));
+        this.graph.registerEdge(this.graph.getVertices().get(1), this.graph.getVertices().get(6));
+        this.graph.registerEdge(this.graph.getVertices().get(2), this.graph.getVertices().get(8));
+        this.graph.registerEdge(this.graph.getVertices().get(3), this.graph.getVertices().get(10));
+        this.graph.registerEdge(this.graph.getVertices().get(4), this.graph.getVertices().get(12));
+
+        this.graph.registerEdge(this.graph.getVertices().get(5), this.graph.getVertices().get(6));
+        this.graph.registerEdge(this.graph.getVertices().get(6), this.graph.getVertices().get(7));
+        this.graph.registerEdge(this.graph.getVertices().get(7), this.graph.getVertices().get(8));
+        this.graph.registerEdge(this.graph.getVertices().get(8), this.graph.getVertices().get(9));
+        this.graph.registerEdge(this.graph.getVertices().get(9), this.graph.getVertices().get(10));
+        this.graph.registerEdge(this.graph.getVertices().get(10), this.graph.getVertices().get(11));
+        this.graph.registerEdge(this.graph.getVertices().get(11), this.graph.getVertices().get(12));
+        this.graph.registerEdge(this.graph.getVertices().get(12), this.graph.getVertices().get(13));
+        this.graph.registerEdge(this.graph.getVertices().get(13), this.graph.getVertices().get(14));
+        this.graph.registerEdge(this.graph.getVertices().get(14), this.graph.getVertices().get(5));
+
+        this.graph.registerEdge(this.graph.getVertices().get(5), this.graph.getVertices().get(15));
+        this.graph.registerEdge(this.graph.getVertices().get(7), this.graph.getVertices().get(16));
+        this.graph.registerEdge(this.graph.getVertices().get(9), this.graph.getVertices().get(17));
+        this.graph.registerEdge(this.graph.getVertices().get(11), this.graph.getVertices().get(18));
+        this.graph.registerEdge(this.graph.getVertices().get(13), this.graph.getVertices().get(19));
+
+        this.graph.registerEdge(this.graph.getVertices().get(15), this.graph.getVertices().get(16));
+        this.graph.registerEdge(this.graph.getVertices().get(16), this.graph.getVertices().get(17));
+        this.graph.registerEdge(this.graph.getVertices().get(17), this.graph.getVertices().get(18));
+        this.graph.registerEdge(this.graph.getVertices().get(18), this.graph.getVertices().get(19));
+        this.graph.registerEdge(this.graph.getVertices().get(19), this.graph.getVertices().get(15));
 
         men = new ArrayList<>();
         lions = new ArrayList<>();
-        men.add(new Man(this.graph.getVertices().get(0).getPosition(), new StrategyRandom()));
-        lions.add(new Lion(this.graph.getVertices().get(1).getPosition(), new StrategyRandom()));
+        men.add(new Man(this.graph.getVertices().get(0).getPosition(), new StrategyRunAwayGreedy()));
+        lions.add(new Lion(this.graph.getVertices().get(1).getPosition(), new StrategyAggroGreedy()));
+        lions.add(new Lion(this.graph.getVertices().get(18).getPosition(), new StrategyAggroGreedy()));
+        lions.add(new Lion(this.graph.getVertices().get(13).getPosition(), new StrategyAggroGreedy()));
 
         this.state = new State(men, lions);
+
 
         return this.graph;
     }
@@ -114,9 +172,9 @@ public class CoreController {
         men = new ArrayList<>();
         lions = new ArrayList<>();
         men.add(new Man(this.graph.getVertices().get(0).getPosition(), new StrategyRunAwayGreedy()));
-        lions.add(new Lion(this.graph.getVertices().get(1).getPosition(), new StrategyAggroGreedy()));
-        lions.add(new Lion(this.graph.getVertices().get(18).getPosition(), new StrategyAggroGreedy()));
-        lions.add(new Lion(this.graph.getVertices().get(13).getPosition(), new StrategyAggroGreedy()));
+        lions.add(new Lion(this.graph.getVertices().get(1).getPosition(), new StretegyAggroClever()));
+        lions.add(new Lion(this.graph.getVertices().get(18).getPosition(), new StretegyAggroClever()));
+        lions.add(new Lion(this.graph.getVertices().get(13).getPosition(), new StretegyAggroClever()));
 
         this.state = new State(men, lions);
 

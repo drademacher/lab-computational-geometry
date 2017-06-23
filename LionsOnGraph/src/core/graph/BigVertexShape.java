c1b7992dbcba0526334f912666e78d9dff971cde
Danny Rademacher <danny.rademacher@gmail.com>
2017-06-23 23:35:40 +0200

    shape classes

---
+++ LionsOnGraph/src/core/graph/BigVertexShape.java
@@ -0,0 +1,13 @@
+package core.graph;
+
+import core.util.Point;
+
+/**
+ * Created by Danny on 23.06.2017.
+ */
+public class BigVertexShape extends BigVertex {
+
+    public BigVertexShape(int id, Point coordinates) {
+        super(id, coordinates);
+    }
+}

fc5255c7a54f366c6e089e91a8ae010d557ac4c7
JENS\Jens <je.harder@gmail.com>
2017-06-25 14:39:34 +0200

    #9 DoNothing strategy

--- LionsOnGraph/src/strategy/StrategyDoNothing.java
+++ LionsOnGraph/src/strategy/StrategyDoNothing.java
@@ -12,13 +12,6 @@ import java.util.ArrayList;
 public class StrategyDoNothing implements Strategy {
     @Override
     public Vertex getNextPosition(Entity e, ArrayList<Man> men, ArrayList<Lion> lions) {
-        //TODO implement (at the moment random)
-        ArrayList<Edge> neighborPositions = e.getCurrentPosition().getEdges();
-        int rndInt = Random.getRandomInteger(neighborPositions.size());
-        return neighborPositions.get(rndInt).getNeighbor(e.getCurrentPosition());
+        return  e.getCurrentPosition();
     }
-//    @Override
-//    public Position getNextPosition(Entity e, ArrayList<Man> men, ArrayList<Lion> lions) {
-//        return e.getCurrentPosition();
-//    }
 }

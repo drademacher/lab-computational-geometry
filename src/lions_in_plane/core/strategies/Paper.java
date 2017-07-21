fe613a5d78cc8dc417ae1d8485e0944aa1da487d
JENS\Jens <je.harder@gmail.com>
2017-07-21 15:28:33 +0200

    calc path for 1 lion

--- src/lions_in_plane/core/strategies/Paper.java
+++ src/lions_in_plane/core/strategies/Paper.java
@@ -27,53 +27,104 @@ public class Paper implements Strategy{
 
         //TODO how to get goalposition?
 
-        System.out.println("all lions: "+lions);
+//        System.out.println("all lions: "+lions);
+//
+//        for(int i = 0; i < lions.size(); i++){
+//
+//            List<Lion> subLions = lions.subList(0, Math.min(lions.size(), i+1));
+//            System.out.println("subLions : "+subLions);
+//            result.add(doMove(man, lions.get(i), null));
+//        }
 
-        for(int i = 0; i < lions.size(); i++){
-
-            List<Lion> subLions = lions.subList(0, Math.min(lions.size(), i));
-            System.out.println("subLions : "+subLions);
-//            result.add( >> new path << );
+        result.add(doMove(man, lions.get(0), null));
+        System.out.println("RESULT......all "+result);
+        System.out.println("RESULT......first "+result.get(0));
+        System.out.println("RESULT......first.size "+result.get(0).length);
+        for(int i = 0; i < result.get(0).length; i++){
+            System.out.println(result.get(0)[i]);
         }
         return result;
     }
 
     /*ASSUME
     *
-    * delta * 1 + epsilon)  ==  man.getSpeed()
+    * delta * (1 + epsilon) ==  man.getSpeed()
     * delta                 ==  lion.getSpeed()
     * saveRadius            == ???   (for now 3* lion.getSpeed())
     *
     */
-    private Point doMove(Man man, Lion lion, Point goalPosition){
+    private Point[] doMove(Man man, Lion lion, Point[] prevPath) {
+
         this.radiusMan = man.getSpeed();
         this.saveRadius = 3* lion.getSpeed();
 
+        Point goalPosition = new Point(0, 0);//TODO
+
+        Point[] curPath = new Point[0];
+
+        //init: only 1 lion -> no exiting prevPath
+        if(prevPath == null){
+            curPath = new Point[120];
+            curPath[0] = man.getPosition();
+            for(int i= 1; i <120; i++){
+                System.out.println("curPath in calculation: "+curPath);
+                curPath[i] = goAwayFromLion(curPath[i-1], lion.getPosition());
+            }
+        }
+
+
         if(man.getPosition().distanceTo(lion.getPosition()) >= saveRadius + radiusMan){
-            return goInGoalDirection(man.getPosition(), goalPosition);
+//            return goInGoalDirection(man.getPosition(), goalPosition);
         } else if(!man.getPosition().equals(goalPosition) &&
                 (man.getPosition().distanceTo(lion.getPosition()) >= saveRadius - lion.getSpeed()) &&
+                //TODO parallel, instead of points??
                 (goInGoalDirection(man.getPosition(), lion.getPosition()).distanceTo(lion.getPosition()) >= (lion.getSpeed() + man.getPosition().distanceTo(lion.getPosition())))){
-            return goInGoalDirection(man.getPosition(), goalPosition);
+//            return goInGoalDirection(man.getPosition(), goalPosition);
         } else{
-            return doAvoidanceMove(man.getPosition(), lion.getPosition());
+//            return doAvoidanceMove(man.getPosition(), lion.getPosition());
         }
+
+        System.out.println("return curPath.."+curPath);
+        return curPath;
+    }
+
+    private Point goAwayFromLion(Point curPosition, Point lionPosition){
+
+        System.out.println("##go away");
+        System.out.println("cur: "+curPosition);
+        System.out.println("lion: "+lionPosition);
+        Point vector = new Point(lionPosition.getX() - curPosition.getX(), lionPosition.getY() - curPosition.getY());
+        System.out.println("vec: "+vector);
+        double vectorLength = vector.length();
+
+        System.out.println("length: "+vectorLength);
+
+        Point unitVector = vector.mul(1/vectorLength);
+
+        System.out.println("unit: "+unitVector);
+        Point stepVector = unitVector.mul(-radiusMan);
+
+        System.out.println("stepVec: "+stepVector);
+
+        Point result = curPosition.add(stepVector);
+        System.out.println("result: "+result);
+        return result;
     }
 
     // free move and escape move
-    private Point goInGoalDirection(Point manPosition, Point goalPosition){
-        Point vector = new Point(goalPosition.getX() - manPosition.getX(), goalPosition.getY() - manPosition.getY());
+    private Point goInGoalDirection(Point curPosition, Point goalPosition){
+        Point vector = new Point(goalPosition.getX() - curPosition.getX(), goalPosition.getY() - curPosition.getY());
         double vectorLength = vector.length();
 
         Point unitVector = vector.mul(1/vectorLength);
         Point stepVector = unitVector.mul(radiusMan);
 
-        return manPosition.add(stepVector);
+        return curPosition.add(stepVector);
     }
 
     // avoidance move
-    private Point doAvoidanceMove(Point manPosition, Point lionPosition){
-        Point [] intersections = getIntersectionPoints(manPosition, radiusMan, lionPosition, saveRadius);
+    private Point doAvoidanceMove(Point curPosition, Point lionPosition){
+        Point [] intersections = getIntersectionPoints(curPosition, radiusMan, lionPosition, saveRadius);
         return intersections[0];//TODO need counterclockwise point
     }
 

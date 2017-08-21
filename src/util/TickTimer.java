0753d47dbd38a01a8c286992d8741bf87613bc7b
Danny Rademacher <danny.rademacher@gmail.com>
2017-08-21 13:45:56 +0200

    introducing tick timer

---
+++ src/util/TickTimer.java
@@ -0,0 +1,62 @@
+package util;
+
+import javafx.animation.AnimationTimer;
+
+import java.util.ArrayList;
+
+public class TickTimer {
+    private static TickTimer instance;
+
+    private ArrayList<Ticker> tickers;
+    private AnimationTimer animationTimer;
+
+
+    private int passedTicks = 0;
+    private double lastNanoTime = System.nanoTime();
+    private double time = 0;
+
+    private final double FPS = 60.0;
+
+
+
+    private TickTimer() {
+        tickers = new ArrayList<>();
+
+        animationTimer = new AnimationTimer() {
+            @Override
+            public void handle(long currentNanoTime) {
+                // calculate time since last update.
+                time += (currentNanoTime - lastNanoTime) / 1000000000.0;
+                lastNanoTime = currentNanoTime;
+                passedTicks = (int) Math.floor(time * FPS);
+                time -= passedTicks / FPS;
+                if (passedTicks >= 1) {
+                    for (Ticker ticker : tickers) {
+                        ticker.action();
+                    }
+                }
+            }
+        };
+        animationTimer.start();
+    }
+
+    public static TickTimer getInstance() {
+        if (instance == null) {
+            instance = new TickTimer();
+        }
+
+        return instance;
+    }
+
+    public interface Ticker {
+        void action();
+    }
+
+    public void addTicker(Ticker ticker) {
+        tickers.add(ticker);
+    }
+
+    public void removeTicker(Ticker ticker) {
+        tickers.removeIf(ticker1 -> ticker1.equals(ticker));
+    }
+}

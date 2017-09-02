bc90a3a68ff9de42728d8f93ac974015e28757e8
Danny Rademacher <danny.rademacher@gmail.com>
2017-09-03 00:38:02 +0200

    view help text demo
    fix #94

--- src/lions_and_men/applet_two/Controller.java
+++ src/lions_and_men/applet_two/Controller.java
@@ -30,6 +30,7 @@ public class Controller {
     private Timer player;
 
     private ZoomScrollPane zoomScrollPane;
+    private Label helpText = new Label();
     private HBox buttonBar;
     private Button helpToggleButton = new Button("Help");
     private Button appletToggleButton = new Button("Switch Application");
@@ -45,10 +46,12 @@ public class Controller {
     private VisualizedCoreController coreController = new VisualizedCoreController();
 
     private Stage stage;
+    private BorderPane root;
 
 
     public Controller(Stage stage, BorderPane root) {
         this.stage = stage;
+        this.root = root;
 
         zoomScrollPane = new ZoomScrollPane();
         root.setCenter(zoomScrollPane);
@@ -77,8 +80,26 @@ public class Controller {
      */
     private void initModeButton() {
         helpToggleButton.setStyle("-fx-font-style: italic");
-        appletToggleButton.setStyle("-fx-font-style: italic");
+        helpToggleButton.setOnAction(event -> {
+            if (root.getCenter() == helpText) {
+                root.setCenter(zoomScrollPane);
+            } else {
+                root.setCenter(helpText);
+            }
+        });
+        helpText.setWrapText(true);
+        helpText.setStyle("-fx-font-family: \"Verdana\"; -fx-font-size: 20; -fx-text-fill: black;");
+        helpText.setPadding(new Insets(50, 50, 50, 50));
 
+        helpText.setText("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. \n" +
+                "\n" +
+                "Duis autem vel eum <b>iriure</b> dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. \n" +
+                "\n" +
+                "Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. \n" +
+                "\n" +
+                "Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. ");
+
+        appletToggleButton.setStyle("-fx-font-style: italic");
         appletToggleButton.setOnAction(event -> {
             Main.switchStage();
         });
